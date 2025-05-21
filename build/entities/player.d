74d045f31e1d56573a186c8e6ed34de379f0cd8b
snacj <bbmelvin@gmx.de>
2025-05-21 20:52:03 +0200

    removed build files

--- build/entities/player.d
+++
@@ -1,7 +0,0 @@
-build/entities/player.o: src/entities/player.cpp src/entities/player.h \
- src/entities/../utils/globals.h src/entities/../utils/textureLoader.h \
- src/entities/../utils/textureLoader.h
-src/entities/player.h:
-src/entities/../utils/globals.h:
-src/entities/../utils/textureLoader.h:
-src/entities/../utils/textureLoader.h:
--- build/main.d
+++
@@ -1,8 +0,0 @@
-build/main.o: src/main.cpp src/main/application.h \
- src/main/../entities/player.h src/main/../entities/../utils/globals.h \
- src/main/../entities/../utils/textureLoader.h src/utils/globals.h
-src/main/application.h:
-src/main/../entities/player.h:
-src/main/../entities/../utils/globals.h:
-src/main/../entities/../utils/textureLoader.h:
-src/utils/globals.h:
